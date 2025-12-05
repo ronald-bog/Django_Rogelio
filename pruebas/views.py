0fc35d638ebb1d472e427fff60461328708ec68e
Ronald Espitia <ronald79@gmail.com>
2025-12-04 22:22:44 -0500

    Class december 4

--- pruebas/views.py
+++ pruebas/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def saludar(request):
+    return HttpResponse('!!! Nuestra primera aplicacion con Django !!!')
+
+def sumar(request):
+    n1 = 10
+    n2 = 15
+    return HttpResponse(n1 + n2)
+
+def notFound(request):
+    return HttpResponse('*** LA RUTA NO EXISTE ***')
