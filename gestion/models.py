bc1531f23a128a3938c8a5b710db0c1a3fa5192f
Ronald Espitia <ronald79@gmail.com>
2025-12-26 22:07:33 -0500

    class dec 26

--- gestion/models.py
+++ gestion/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+# Modelo para la tabla 'Category'
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT
+    )
+
+    def __str__(self):
+        return self.name
+
+
+
