381d6676e6f43c9dd416ca8271b42d7ece87680d
Ronald Espitia <ronald79@gmail.com>
2026-02-26 19:35:15 -0500

    Clase febrero 25

--- gestion/urls.py
+++ gestion/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('products/', views.product_list),
     path('creacion/', views.product_create),
     path('patch/<int:id>', views.product_update_patch),
-    path('borrar/<int:id>', views.deleteProduct)
+    path('borrar/<int:id>', views.deleteProduct),
+    path('obtener/<int:pk>', views.unSoloProducto),
+    path('total/<int:pk>', views.product_update_put),
 ]
--- gestion/views.py
+++ gestion/views.py
@@ -9,22 +9,41 @@ from django.shortcuts import get_object_or_404
 from gestion.models import Product, Category
 
 
-# Create your views here.
+# LISTAR PRODUCTOS (GET)
 def product_list(request):
     products = Product.objects.all()
     data = []
     for prod in products:
         data.append(
             {
-            'id':prod.id,
-            'name':prod.name,
-            'price':prod.price,
-            'stock':prod.stock,
-            'active':prod.active,
-            'created_at':prod.created_at,
-        }
+                'id': prod.id,
+                'name': prod.name,
+                'price': prod.price,
+                'stock': prod.stock,
+                'active': prod.active,
+                'created_at': prod.created_at,
+            }
         )
     return JsonResponse(data, safe=False)
+
+
+# Obtener un solo producto por su ID (GET)
+def unSoloProducto(request, pk):
+    if request.method != 'GET':
+        return JsonResponse({'error': 'Metodo no es valido'}, status=405)
+
+    producto = get_object_or_404(Product, pk=pk)
+
+    data = {
+        'id': producto.id,
+        'nombre': producto.name,
+        'price': producto.price,
+        'stock': producto.stock,
+        'active': producto.active,
+        'created_at': producto.created_at,
+    }
+
+    return JsonResponse(data)
 
 
 # CREAR PRODUCTO (POST)
@@ -38,19 +57,20 @@ def product_create(request):
     body = json.loads(request.body)
 
     # Validamos si la categoria que viene en el json es correcta
-    category = get_object_or_404(Category,  id=body['category_id'])
+    category = get_object_or_404(Category, id=body['category_id'])
 
     product = Product.objects.create(
-        name = body['name'],
-        price = body['price'],
-        stock = body['stock'],
-        active = body['active'], ### body.get('active', 1 )
-        #created_at = body['created_at'],
-        category = category
+        name=body['name'],
+        price=body['price'],
+        stock=body['stock'],
+        active=body['active'],  ### body.get('active', 1 )
+        # created_at = body['created_at'],
+        category=category
     )
     return JsonResponse(
         {'message': 'Producto se creo exitosamente', 'id': product.id}, status=201
     )
+
 
 # name, precio, stock, active 0, created_at, category_id
 
@@ -71,11 +91,40 @@ def product_update_patch(request, id):
     product.active = body.get('active', product.active)
 
     if 'category_id' in body:
-        product.category = get_object_or_404(Category,  id=body['category_id'])
+        product.category = get_object_or_404(Category, id=body['category_id'])
 
     product.save()
 
     return JsonResponse({'message': 'Producto actualizado exitosamente'})
+
+
+# ACTUALIZACION TOTAL (PUT)
+@csrf_exempt
+def product_update_put(request, pk):
+    if request.method != 'PUT':
+        return JsonResponse({'error': 'Metodo no es valido'})
+
+    product = get_object_or_404(Product, pk=pk)
+
+    body = json.loads(request.body)
+
+    # validar que todos los campos obligatorios esten presentes
+    camposRequeridos = ['name', 'price', 'stock', 'active', 'category_id']
+
+    for campo in camposRequeridos:
+        if campo not in body:
+            return JsonResponse({'error': f'campo requeirdo no presente {campo}'})
+
+    # Cambios en la base de datos, obligatorio notacion de corchetes []
+    product.name = body['name']
+    product.price = body['price']
+    product.stock = body['stock']
+    product.active = body['active']
+    product.category = get_object_or_404(Category, pk=body['category_id'])
+
+    product.save()
+
+    return JsonResponse({'message': 'Producto actualizado totalmente con un PUT'})
 
 
 # ELIMINAR PRODUCTO (DELETE)
@@ -85,15 +134,9 @@ def deleteProduct(request, id):
     if request.method != 'DELETE':
         return JsonResponse({'error': 'Metodo no es valido'})
 
-    product = get_object_or_404(Product, pk=id) # aqui se obtiene el producto, mas no se borra aun
+    product = get_object_or_404(Product, pk=id)  # aqui se obtiene el producto, mas no se borra aun
 
-    product.delete() # aqui se borra el producto
+    product.delete()  # aqui se borra el producto
 
-    return JsonResponse({'mensaje': 'Producto eliminado con exito'}) # se envia la respuesta al cliente (usuario) o a quien hizo la peticion
-
-
-
-
-
-
-
+    return JsonResponse({
+        'mensaje': 'Producto eliminado con exito'})  # se envia la respuesta al cliente (usuario) o a quien hizo la peticion
